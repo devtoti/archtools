793f87a3e146992e861ca15fd5aecebab15b8454
devtoti <96400225+devtoti@users.noreply.github.com>
2022-08-27 18:45:44 -0500

    Unit conversion working

--- components/ScaleCalculator.js
+++ components/ScaleCalculator.js
@@ -2,93 +2,74 @@ import styles from '../styles/scales.module.scss'
 import { useEffect, useState, useRef } from 'react'
 import ScaleFactor from './scaleFactor'
 
-
-// export async function getStaticProps(context) {
-//   return {
-//     props: {}, // will be passed to the page component as props
-//   }
-// }
-
-
 export default function ScaleCalculator(props) {
 
   const userRef = useRef()
   const scaledRef = useRef()
 
-  const [scaledUnits, setScaledUnits] = useState('cm')
 
 
-  const [userUnits, setUserUnits] = useState('m')
-  const [userValue, setUserValue] = useState(6)
-  const [numerator, setNumerator] = useState(3)
-  const [denominator, setDenominator] = useState(12)
+  const [scaledUnits, setScaledUnits] = useState('m')
+  const [userUnits, setUserUnits] = useState('km')
+  const [userValue, setUserValue] = useState(37)
+
   const [scaleResult, setScaleResult] = useState(0)
-  const [simplifiedRatio, setSimplifiedRatio] = useState({n:0, d:0})
-
-
-  let ratio = numerator / denominator
+  const [numerator, setNumerator] = useState(1)
+  const [denominator, setDenominator] = useState(125)
+  const [simplifiedRatio, setSimplifiedRatio] = useState({ n: 0, d: 0 })
 
 
   useEffect(() => {
-    let result = userValue * ratio
-    result = Number(result.toFixed(4))
-    setScaleResult(result)
-  }, [userValue, numerator, denominator])
 
-  // const changeUnits = (val, un) => {
-  //   let un = {
-  //     "mm": 1,
-  //     "cm": 1e-1,
-  //     "m": 1e-3,
-  //     "km": 1e-6,
-  //   }
-  // }
+  }, [])
 
-  // let newUnits = getEquivalence(scaleResult, scaledUnits)
+  useEffect(() => {
+    let ratio = +numerator / +denominator
+    let userValue2 = Number(userValue)
+    console.log(userValue2)
+    let scaledResult = userValue2 * ratio
+    // scaledResult = Number(scaledResult.toFixed(4))
+
+    // console.log(newUnit)
+
+    let newValue = changeUnits(scaledResult, userUnits, scaledUnits)
+    // console.log(newUnit)
+    console.table({ scaleResult, numerator, denominator, ratio, userValue, newValue, userUnits, scaledUnits })
+    setScaleResult(newValue)
+  }, [userValue, numerator, denominator, userUnits, scaledUnits])
+
+
+  const changeUnits = (value, initial_unit, desired_unit) => {
+    value = Number(value)
+    console.log(initial_unit, desired_unit)
+    let conversions = {
+      "mm": 1,
+      "cm": 1e1,
+      "m": 1e3,
+      "km": 1e6,
+    }
+
+    let RATIO_IN_MM = Number(conversions[initial_unit]) / Number(conversions[desired_unit])
+
+    console.log(value, RATIO_IN_MM)
+    let newValue = value * RATIO_IN_MM;
+    return newValue
+  }
+
 
 
   const ratioTo1 = (n, d) => {
     n = parseInt(n)
     d = parseInt(d)
     let result = {};
-    // if (n < d)
-    // if (n < d) {
-      result = {
-        "n":(n * 1) / n,
-        "d": (d * 1) / n
-      }
-      setSimplifiedRatio(result)
-      console.log(result)
-      return result
-  
+    result = {
+      "n": (n * 1) / n,
+      "d": (d * 1) / n
+    }
+    // setSimplifiedRatio(result)
+    return result
+
   }
-
-
-  // const scaleNumb = (value, n1, n2) => {
-  //   value = parseInt(value)
-  //   n1 = parseInt(n1)
-  //   n2 = parseInt(n2)
-  //   let result = value * (n1 / n2)
-  //   result = Number(result.toFixed(4))
-  //   setScaleResult(result)
-  //   return result
-  // }
-
-
-  // let proof = scaleNumb(userValue, numerator, denominator)
-
-  // const simplifiedScale = (val, numer, denom) => {
-  //   let result;
-  //   if (numer > denom) {
-  //     let n = 1 / numer
-  //     result = (numer * n) + ":" + (denom * n);
-  //     return result
-  //   }
-  //   else if (denom > numer && numer / denom !== 1) {
-  //     result = (numer * (1 / val)) / (denom * (1 / val))
-  //   }
-  //   return result
-  // }
 
 
 
@@ -97,37 +78,33 @@ export default function ScaleCalculator(props) {
     <div className={styles.container}>
       <h1>SCALE CALC</h1>
       <label htmlFor="user">Distance</label>
-      <input type="number" id="user" placeholder="user input" value={userValue} onChange={(e) => setUserValue(e.target.value)} />
-      {/* <select id="units" ref={userRef} onChange={() => setUserUnits(userRef.current)}> */}
+      <input type="number" id="user" placeholder="user input" value={userValue} onChange={(e) => setUserValue(Number(e.target.value))} />
+
       <select id="units" ref={userRef} onChange={() => setUserUnits(userRef.current.value)}>
         <option value="mm">milimeters</option>
         <option value="cm">centimeters</option>
-        <option value="m">meters</option>
+        <option value="m" selected>meters</option>
         <option value="km">kilometers</option>
       </select>
       <br></br>
       <div className={styles.inputs__scales} onChange={() => ratioTo1(numerator, denominator)}>
 
-        <input type="number" id="real-length" placeholder="real length" value={numerator} onChange={(e) => setNumerator(e.target.value)} />
+        <input type="number" id="real-length" placeholder="real length" value={numerator} onChange={(e) => setNumerator(Number(e.target.value))} />
         <h4>:</h4>
-        <input type="number" id="scale-length" placeholder="scale length" value={denominator} onChange={(e) => setDenominator(e.target.value)} />
-        {/* <label htmlFor="units2">Units</label>
-        <select id="units" ref={scaledRef} onChange={() => setScaledUnits(scaledRef.current.value)}>
+        <input type="number" id="scale-length" placeholder="scale length" value={denominator} onChange={(e) => setDenominator(Number(e.target.value))} />
+        <label htmlFor="desiredUnits">Units</label>
+        <select id="desiredUnits" ref={scaledRef} onChange={() => setScaledUnits(scaledRef.current.value)}>
           <option value="mm">milimeters</option>
           <option value="cm">centimeters</option>
-          <option value="m">meters</option>
+          <option value="m" selected>meters</option>
           <option value="km">kilometers</option>
-        </select> */}
+        </select>
       </div>
       <div className="results">
 
-        <h3>{`${userValue} * ${numerator} / ${denominator} =` + scaleResult + userUnits}</h3>
+        <h3>{`${userValue} * ${numerator} / ${denominator} =` + scaleResult.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " " + scaledUnits}</h3>
 
-        {/* <h6>{getEquivalence(scaleNumb(userValue, numerator, denominator), userUnits, ratio, scaledUnits)}</h6>
- */}
-
-
-        <h4>scale {numerator}:{denominator} ( {simplifiedRatio.n.toFixed(2)} : {simplifiedRatio.d.toFixed(2)})</h4>
+        <h4>scale {numerator}:{denominator}</h4>
 
       </div>
       <h6>{scaleResult}</h6>
