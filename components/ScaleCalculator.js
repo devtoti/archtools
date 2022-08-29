03d9eb7287d06212103c986749e8127d4a160709
devtoti <96400225+devtoti@users.noreply.github.com>
2022-08-28 20:13:22 -0500

    Finish html tree structure - ScaleCalculator

--- components/ScaleCalculator.js
+++ components/ScaleCalculator.js
@@ -1,7 +1,8 @@
 import styles from '../styles/scales.module.scss'
 import { useEffect, useState, useRef } from 'react'
 import ScaleFactor from './scaleFactor'
-
+// import { Fab } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 export default function ScaleCalculator(props) {
 
   const userRef = useRef()
@@ -20,7 +21,9 @@ export default function ScaleCalculator(props) {
 
   let ratio = +numerator / +denominator
   let sameUnitsResult = userValue * ratio
-
+  const suggScales = [
+    "1/10", "1/20", "1/50", "1/100", "1/200", "1/500"
+  ]
   useEffect(() => {
 
   }, [])
@@ -28,14 +31,8 @@ export default function ScaleCalculator(props) {
   useEffect(() => {
     let ratio = +numerator / +denominator
     let userValue2 = Number(userValue)
-    console.log(userValue2)
     let scaledResult = userValue2 * ratio
-    // scaledResult = Number(scaledResult.toFixed(4))
-
-    // console.log(newUnit)
-
     let newValue = changeUnits(scaledResult, userUnits, scaledUnits)
-    // console.log(newUnit)
     console.table({ scaleResult, numerator, denominator, ratio, userValue, newValue, userUnits, scaledUnits })
     setScaleResult(newValue)
   }, [userValue, numerator, denominator, userUnits, scaledUnits])
@@ -53,7 +50,7 @@ export default function ScaleCalculator(props) {
 
     let RATIO_IN_MM = Number(conversions[initial_unit]) / Number(conversions[desired_unit])
 
-    console.log(value, RATIO_IN_MM)
+    // console.log(value, RATIO_IN_MM)
     let newValue = value * RATIO_IN_MM;
     return newValue
   }
@@ -68,7 +65,6 @@ export default function ScaleCalculator(props) {
       "n": (n * 1) / n,
       "d": (d * 1) / n
     }
-    // setSimplifiedRatio(result)
     return result
 
   }
@@ -85,56 +81,100 @@ export default function ScaleCalculator(props) {
       </div>
       <div className={styles.body}>
         <h5 className={styles.instruction}>Specify distance to be converted</h5>
-      <label htmlFor="real-length">Distance</label>
-      <input type="number" id="real-length" placeholder="user input" value={userValue} onChange={(e) => setUserValue(Number(e.target.value))} />
+        <section>
+          <article>
+            <label htmlFor="real-length">Distance</label>
+            <input type="number" id="real-length" placeholder="user input" value={userValue} onChange={(e) => setUserValue(Number(e.target.value))} />
 
-      <label htmlFor="initial-units">Units</label>
-      <select id="initial-units" ref={userRef} onChange={() => setUserUnits(userRef.current.value)}>
-        <optgroup label="International">
-          <option value="mm">milimeters</option>
-          <option value="cm">centimeters</option>
-          <option value="m" selected>meters</option>
-          <option value="km">kilometers</option>
-        </optgroup>
-        <optgroup label="Imperial">
-          <option value="in">inches</option>
-          <option value="ft">feet</option>
-          <option value="yd">yards</option>
-          <option value="mi">miles</option>
-        </optgroup>
-      </select>
-      <br></br>
+          </article>
+          <article>
+
+            <label htmlFor="initial-units">Units</label>
+            <select id="initial-units" ref={userRef} onChange={() => setUserUnits(userRef.current.value)}>
+              <optgroup label="International">
+                <option value="mm">milimeters</option>
+                <option value="cm">centimeters</option>
+                <option value="m" selected>meters</option>
+                <option value="km">kilometers</option>
+              </optgroup>
+              <optgroup label="Imperial">
+                <option value="in">inches</option>
+                <option value="ft">feet</option>
+                <option value="yd">yards</option>
+                <option value="mi">miles</option>
+              </optgroup>
+            </select>
+          </article>
+        </section>
+        <br></br>
+
         <h5 className={styles.instruction}>Pick your desired scale</h5>
-      <div className={styles.inputs__scales} onChange={() => ratioTo1(numerator, denominator)}>
-        <input type="number" id="real-length" placeholder="real length" value={numerator} onChange={(e) => setNumerator(Number(e.target.value))} />
-        <h4>:</h4>
-        <input type="number" id="scale-length" placeholder="scale length" value={denominator} onChange={(e) => setDenominator(Number(e.target.value))} />
-        <label htmlFor="desiredUnits">Units</label>
-        <select id="desiredUnits" ref={scaledRef} onChange={() => setScaledUnits(scaledRef.current.value)}>
-          <optgroup label="International">
-            <option value="mm">milimeters</option>
-            <option value="cm">centimeters</option>
-            <option value="m" selected>meters</option>
-            <option value="km">kilometers</option>
-          </optgroup>
-          <optgroup label="Imperial">
-            <option value="in">inches</option>
-            <option value="ft">feet</option>
-            <option value="yd">yards</option>
-            <option value="mi">miles</option>
-          </optgroup>
-        </select>
-      </div>
-      </div>
-      <div className={styles.results}>
-  
-        <h3>{`${userValue + " " + userUnits}* ${numerator} / ${denominator} =` + sameUnitsResult + " " + userUnits}</h3>
+        <section className={styles.grid}>
+          <article>
+            <label htmlFor="nominator">Scale</label>
+            <input type="number" id="nominator" placeholder="real length" value={numerator} onChange={(e) => setNumerator(Number(e.target.value))} />
+          </article>
+          <h4>:</h4>
 
-        <h4>scale {numerator}:{denominator}</h4>
+          <article>
+            <input type="number" id="denominator" placeholder="scale length" value={denominator} onChange={(e) => setDenominator(Number(e.target.value))} />
+          </article>
+
+          <article>
+            <label htmlFor="desiredUnits">Units</label>
+            <select id="desiredUnits" ref={scaledRef} onChange={() => setScaledUnits(scaledRef.current.value)}>
+              <optgroup label="International">
+                <option value="mm">milimeters</option>
+                <option value="cm">centimeters</option>
+                <option value="m" selected>meters</option>
+                <option value="km">kilometers</option>
+              </optgroup>
+              <optgroup label="Imperial">
+                <option value="in">inches</option>
+                <option value="ft">feet</option>
+                <option value="yd">yards</option>
+                <option value="mi">miles</option>
+              </optgroup>
+            </select>
+          </article>
+        </section>
+
+              <h6>Or choose one of the scales below</h6>
+          <section>
+            <article className={styles.suggScales}>
+
+              {suggScales.map((scale, ix) => <div key={ix}>{scale}</div>)}
+              <hr />
+            </article>
+          </section>
+
+
       </div>
-      <h1>
-        {scaleResult.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + scaledUnits}</h1>
+        <h5 className={styles.instruction}>Evaluate your results</h5>
+      <div className={styles.results}>
+        <h5>{`${userValue + " " + userUnits}* ${numerator} / ${denominator} =` + sameUnitsResult + " " + userUnits}</h5>
+        <h2>{userValue + " " + userUnits}</h2>
+        <h6>scale {numerator}:{denominator}</h6>
+        <canvas></canvas>
+        <article>
+
+        <h1>
+          {scaleResult.toFixed(4).toString().replace(/\B(?=(\d{3})+(?!\d))/g, "") + " " + scaledUnits}</h1>
+        <h6>Result</h6>
+        </article>
+        {/* <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab> */}
+      </div>
+      <div className={styles.footer}>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <h6>brought to you by ARC</h6>
+      </div>
     </div>
+
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   const [scaledLength, setScaledLength] = useState(34)
   const [realLength, setRealLength] = useState(0)
   const [hasMounted, setHasMounted] = useState(false);
-  
+
   // useEffect(() => {
   //   setHasMounted(true);
   // }, []);
@@ -26,20 +26,23 @@ export default function Home() {
         <title>ARChTools</title>
         <meta name="description" content="Architecture Widgets" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz@8..144&family=Source+Sans+Pro:wght@300;400&display=swap" rel="stylesheet"></link>
+          </Head>
 
-      <main className={styles.main}>
-        <ScaleCalculator/>
-         {/* <ScaleFactor 
+          <main className={styles.main}>
+            <ScaleCalculator />
+            {/* <ScaleFactor 
       realLength={!realLength ? "" : realLength}
       setRealLength={setRealLength}
       scaledLength={!scaledLength ? "" : scaledLength}
       setScaledLength={setScaledLength}
 
        /> */}
-      </main>
+          </main>
 
-      <footer className={styles.footer}></footer>
-    </div>
-  )
+          <footer className={styles.footer}></footer>
+        </div>
+        )
 }
